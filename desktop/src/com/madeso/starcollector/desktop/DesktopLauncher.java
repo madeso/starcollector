198effd8538d4be81a127665e82ece342622883c
Gustav <sir.gustav.the.coder@gmail.com>
2017-11-13 22:00:06 +0100

    started converting project to kotlin and new libgdx

---
+++ desktop/src/com/madeso/starcollector/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.madeso.starcollector.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.madeso.starcollector.StarCollectorGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarCollectorGame(), config);
+	}
+}
